fb60f14b3a6450b1b78fb0c34f3e651b8284a20b
keshav <keshavrajkashyap711@gmail.com>
2021-12-07 11:33:14 +0530

    non escaping

--- NonEscaping/ViewController.swift
+++ NonEscaping/ViewController.swift
@@ -11,9 +11,29 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        addTwoNumber()
     }
-
+    
+    func sumOfNumbers(_ num1: Int, andSecondNumber num2:Int,completionHandler:@escaping (_ result:Int) -> Void) {
+        print("step 2")
+        let result = num1 + num2
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.3){
+            print("step 3")
+            completionHandler(result)
+        }
+    }
+    
+   
+    func addTwoNumber(){
+        print("step 1")
+        sumOfNumbers(10, andSecondNumber: 20){ [weak self](result) in
+            guard let _ = self else {return}
+            print("result is \(result)")
+        }
+        print("last step")
+    }
+    
+    
 
 }
 
